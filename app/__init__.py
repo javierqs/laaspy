5e748ca16a0558c0ae2c59adf97230c3629b398e
J <jquarites@gmail.com>
2014-11-14 10:28:08 -0500

    setting everything for DO deploy ...bad deploy

--- app/__init__.py
+++ app/__init__.py
@@ -121,27 +121,3 @@ class Laaspy(object):
 
         return html_content
     lingo_process.exposed = True
-
-def setup_routes():
-    d = cherrypy.dispatch.RoutesDispatcher()
-    d.connect('home', '/', controller= MainController(), action="index")
-    d.connect('model', '/models/{name}', controller=ModelsController(), action="show")
-    d.connect('process', '/models/{name}/process', controller=ModelsController(), action="process", conditions=dict(method=['POST']))
-    dispatcher = d
-    return dispatcher
-
-conf = {
-    '/': {
-        'request.dispatch': setup_routes(),
-        'tools.staticdir.root': os.path.abspath(os.getcwd())
-     }, 
-    '/static': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': './public'
-    }
-}
-app = cherrypy.tree.mount(None, config=conf)
-cherrypy.server.socket_host = '0.0.0.0'
-cherrypy.quickstart(app)
-#cherrypy.server.socket_host = '0.0.0.0'
-#cherrypy.quickstart(Laaspy(), '/', conf)
---
+++ wsgi.py
@@ -0,0 +1,28 @@
+import cherrypy
+from app import *
+
+def setup_routes():
+    d = cherrypy.dispatch.RoutesDispatcher()
+    d.connect('home', '/', controller= MainController(), action="index")
+    d.connect('model', '/models/{name}', controller=ModelsController(), action="show")
+    d.connect('process', '/models/{name}/process', controller=ModelsController(), action="process", conditions=dict(method=['POST']))
+    dispatcher = d
+    return dispatcher
+
+if __name__=='__main__':
+    conf = {
+        '/': {
+            'request.dispatch': setup_routes(),
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+         }, 
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+         }
+    }
+
+    app = cherrypy.tree.mount(None, config=conf)
+    cherrypy.server.socket_host = '127.0.0.1'
+    cherrypy.server.socket_port = 8080
+    cherrypy.quickstart(app)
+
